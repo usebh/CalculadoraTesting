384110fd773331bd2f56106a7fc14c46fee35600
Eusebio <usebh@hotmail.com>
2020-05-09 09:53:45 +0100

    Creada funcion raiz para test

---
+++ Calculadora/RaizTest.java
@@ -0,0 +1,14 @@
+package Calculadora;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RaizTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
