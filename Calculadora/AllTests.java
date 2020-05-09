06fae209644957ed0ea8e3a0faa788418507925d
Eusebio <usebh@hotmail.com>
2020-05-09 10:52:42 +0100

    se crea suiteClass para ejecutar todos los test

---
+++ Calculadora/AllTests.java
@@ -0,0 +1,11 @@
+package Calculadora;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DivisionTest.class, MultiplicacionTest.class, RaizTest.class, RestaTest.class, SumaTest.class })
+public class AllTests {
+
+}
