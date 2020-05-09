1adb40d3a3b59637b09dcc61089ae3ca234c11de
Eusebio <usebh@hotmail.com>
2020-05-09 10:44:08 +0100

    Se añaden mas test para la raiz cuadrada

--- Calculadora/RaizTest.java
+++ Calculadora/RaizTest.java
@@ -23,5 +23,38 @@ public class RaizTest {
 		assertEquals(6.7082039,resultado,5);
 		
 	}
-
+	@Test
+	public void raiztest1() {
+        CalculadoraClass junit = new CalculadoraClass();
+		
+		//arrange
+		
+		float resultado;
+		
+		//act
+		
+		resultado = junit.raiz(143);
+		
+		//assert
+		
+		assertEquals(11.9582607,resultado,5);
+		
+	}
+	@Test
+	public void raiztest2() {
+        CalculadoraClass junit = new CalculadoraClass();
+		
+		//arrange
+		
+		float resultado;
+		
+		//act
+		
+		resultado = junit.raiz(432);
+		
+		//assert
+		
+		assertEquals(20.7846096,resultado,5);
+		
+	}
 }
